e08691fcd3650db29bff872e14fb62aa67643f94
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-04 11:49:37 +0530

    Repeat till player reaches winning 100

--- src/snakeladder/SnakeAndLadderSimulator.java
+++ src/snakeladder/SnakeAndLadderSimulator.java
@@ -10,34 +10,51 @@ public class SnakeAndLadderSimulator {
 		final int NOPLAY = 0;
 		final int LADDER = 1;
 		final int SNAKE = 2;
+		int WINNING_POSITION = 100;
 		
 		// Variables
-		
 		int pos1 = 0;
 		int counter = 0;
 		
-		// Dice roll
 		
-		int roll = (int) ( Math.floor( Math.random()*10 ) %6 ) + 1 ;
+		while ( pos1 != WINNING_POSITION )
+		{
 		
-		// Options for Movement
+			// Dice roll
+			int roll = (int) ( Math.floor( Math.random()*10 ) %6 ) + 1 ;
 		
-		int option = (int) Math.floor( Math.random()*10 ) %3;
-		switch(option)
+			// Options for Movement
+			int option = (int) Math.floor( Math.random()*10 ) %3;
+			switch(option)
+				{
+					case NOPLAY:
+						counter = 0;
+						break;
+					case LADDER:
+						counter = roll;
+						break;
+					case SNAKE:
+						counter = -roll;
+						break;
+				}
+			
+			// Movement Calculation
+			pos1 = pos1 + counter;
+		
+			// Movement below Start Position
+			if ( pos1 < NOPLAY )
 			{
-				case NOPLAY:
-					counter = 0;
-					break;
-				case LADDER:
-					counter = roll;
-					break;
-				case SNAKE:
-					counter = -roll;
-					break;
+				pos1 = NOPLAY;
+			}
+			
+			// Movement above Winning Position
+			else if ( pos1 > WINNING_POSITION )
+			{
+				pos1 = pos1 - counter;
 			}
 				
-		System.out.println("According to option your movement will be "+counter);
-		
 			
+		}	
+	System.out.println("Player 1 has won by reaching the winning position "+pos1);
 	}
 }
